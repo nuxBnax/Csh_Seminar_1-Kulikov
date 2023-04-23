dd88ad67b601f4482b5b1c04fdfb5ccc9d39391f
Kulikov Alexander <shouter84@yandex.ru>
2023-04-23 22:35:16 +0300

    Added solution of Task_6

---
+++ Task_6/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 6: 
+
+// Напишите программу, которая на вход принимает число 
+// и выдаёт, является ли число чётным (делится ли оно
+// на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Enter ANY number you want, PLEASE!");
+int num = Convert.ToInt32(System.Console.ReadLine());
+
+if (num % 2 == 0)
+{
+    Console.WriteLine("YES, the number you have gust entered is EVEN!");
+}
+else 
+{
+    Console.WriteLine("NO, the number you have gust entered is ODD!");
+}
